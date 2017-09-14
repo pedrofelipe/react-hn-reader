3f17a4b0ab94b199f611774f15b1f67c7cce0a8c
Pedro Felipe <pedro@pedrofelipe.com.br>
2017-09-13 21:16:11 -0300

    Change folder structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
+
+import reducers from './reducers';
+
 import './index.css';
-import App from './App';
+import App from './containers/app/app';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const client = axios.create({
+  baseURL:'https://hacker-news.firebaseio.com/v0',
+  responseType: 'json'
+});
+
+const store = createStore(
+  reducers,
+  applyMiddleware(
+    axiosMiddleware(client),
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
