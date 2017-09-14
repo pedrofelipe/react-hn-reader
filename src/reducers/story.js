85031297a1faf6ff7fa6b6fa8c11fa133ae97f49
Pedro Felipe <pedro@pedrofelipe.com.br>
2017-09-13 21:16:50 -0300

    Create reducers

---
+++ src/reducers/story.js
@@ -0,0 +1,39 @@
+import {
+  REQUEST_STORY_START,
+  REQUEST_STORY_SUCCESS,
+  REQUEST_STORY_FAILURE
+} from '../constants'
+
+const initialState = {
+  list: [],
+  open: {},
+  isLoading: false,
+  hasFailed: false
+}
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case REQUEST_STORY_START:
+      return {
+        ...state,
+        isLoading: true
+      }
+    case REQUEST_STORY_SUCCESS:
+      return {
+        ...state,
+        list: [
+          ...state.list,
+          action.payload.data
+        ],
+        isLoading: false
+      }
+    case REQUEST_STORY_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        hasFailed: true
+      }
+    default:
+      return state;
+  }
+}
